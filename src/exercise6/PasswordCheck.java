01ffa7e22c55dfa412b5115c22870655371a30c2
VSherova <>
2021-11-17 16:26:15 +0000

    Test3

--- src/exercise6/PasswordCheck.java
+++ src/exercise6/PasswordCheck.java
@@ -4,33 +4,34 @@ import java.util.Scanner;
 
 public class PasswordCheck {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+        Scanner input = new Scanner(System.in) ;
 
         
-        boolean passwordValid = false;
-        do{
-          System.out.println("Enter password: ");
-          String password = input.nextLine();
-          if(password.length() < 8){
-            continue;
+        boolean passwordValid = false ;
+        do {
+          System.out.println("Enter password: ") ;
+          String password = input.nextLine() ;
+          if(password.length() < 8) {
+            continue ;
           }
-          boolean allLettersAndDigits = true;
-          int digitsCount = 0;
-          for (int index = 0; index < password.length(); index++){
-            char symbol = password.charAt(index);
-            boolean letterOrDigit = Character.isLetterOrDigit(symbol);
-            if (!letterOrDigit){
-              allLettersAndDigits = false;
-              break;
+          boolean allLettersAndDigits = true ;
+          int digitsCount = 0 ;
+          for (int index = 0; index < password.length() ; index++) {
+            char symbol = password.charAt(index) ;
+            boolean letterOrDigit = Character.isLetterOrDigit(symbol) ;
+            if (!letterOrDigit) {
+              allLettersAndDigits = false ;
+              break ;
             }
-            boolean digit = Character.isDigit(symbol);
+            
+            boolean digit = Character.isDigit(symbol) ;
             if (digit) {
-              digitsCount++;
+              digitsCount++ ;
             }
           }
-          if (allLettersAndDigits && digitsCount >= 2){
-            passwordValid = true;
+          if (allLettersAndDigits && digitsCount >= 2) {
+            passwordValid = true ;
           }
-        } while (!passwordValid);
+        } while (!passwordValid) ;
     }
 }
