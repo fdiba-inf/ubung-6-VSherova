d9c91b801ddd08555afb7389cd27876321fc9e32
VSherova <>
2021-11-17 16:32:41 +0000

    Test0

--- src/exercise6/PasswordCheck.java
+++ src/exercise6/PasswordCheck.java
@@ -5,32 +5,37 @@ import java.util.Scanner;
 public class PasswordCheck {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-
         
-        boolean passwordValid = false;
-        do{
-          System.out.println("Enter password: ");
+        boolean passwordValid = false; 
+        do {
+          System.out.print("Enter password: ");
           String password = input.nextLine();
-          if(password.length() < 8){
+
+          if (password.length()< 8) {
             continue;
           }
-          boolean allLettersAndDigits = true;
-          int digitsCount = 0;
-          for (int index = 0; index < password.length(); index++){
+
+          boolean allLettersAndDigits = true; 
+          int digitCount = 0; 
+          for (int index = 0; index < password.length(); index ++){
             char symbol = password.charAt(index);
             boolean letterOrDigit = Character.isLetterOrDigit(symbol);
-            if (!letterOrDigit){
+            if (!letterOrDigit) {
               allLettersAndDigits = false;
-              break;
+              break; 
             }
+
             boolean digit = Character.isDigit(symbol);
-            if (digit) {
-              digitsCount++;
+            if (digit){
+              digitCount++;
             }
           }
-          if (allLettersAndDigits && digitsCount >= 2){
+
+          if (allLettersAndDigits && digitCount >= 2){
             passwordValid = true;
           }
         } while (!passwordValid);
+
+        System.out.println("Password valid!: ");
     }
 }
